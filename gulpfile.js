d83621baa366de5b380f67a5a75571d4407719ad
dstyle0210 <dstyle0210@gmail.com>
2017-01-12 16:43:08 +0900

    babel 걷어낸 버전 최종커밋 이게 1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gutil = require('gulp-util');
 var zip = require('gulp-zip');
 
 gulp.task("dist" , function(){
-   return gulp.src(["./src/.babelrc","./src/gulpfile.babel.js","./src/package.json"])
+   return gulp.src(["./src/gulpfile.js","./src/package.json"])
     .pipe( gulp.dest("./dist") )
     .pipe(zip("generator-dstyle.zip"))
     .pipe( gulp.dest("./dist") );
